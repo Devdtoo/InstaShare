7f2410898db560b20351c8eb704f44950e603242
Devdtoo <devdtoo@gmail.com>
2019-11-13 03:21:10 +0530

    1. v2 Done Login/Registration Implemented

---
+++ app/src/main/java/com/example/instashare/LoginActivity.java
@@ -0,0 +1,143 @@
+package com.example.instashare;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    /*AnimationDrawable animationDrawable;
+    LinearLayout linear_start;*/
+
+    EditText email, password;
+    TextView txt_forgot_password, txt_fb_login, txt_signUp;
+    Button btn_login;
+
+    ProgressDialog progressDialog;
+
+    FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is already logged In... This is to bypass Login screen to MainScreen
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+//        For Animation on Gradient of Splash
+        /*linear_start = findViewById(R.id.relative_start);
+        animationDrawable =(AnimationDrawable)linear_start.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);*/
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        txt_forgot_password = findViewById(R.id.forgot_password);
+        txt_fb_login = findViewById(R.id.facebook_login);
+        txt_signUp = findViewById(R.id.txt_signup);
+        btn_login = findViewById(R.id.btn_login);
+        auth = FirebaseAuth.getInstance();
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog = new ProgressDialog(LoginActivity.this);
+                progressDialog.setMessage("Please wait..");
+                progressDialog.show();
+
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+                Log.i("Login Btn", "Button clicked");
+
+                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "All the fields are required", Toast.LENGTH_SHORT).show();
+                } else if (txt_password.length() < 6) {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "Password must be of at least 6 characters", Toast.LENGTH_SHORT).show();
+                } else {
+                    auth.signInWithEmailAndPassword(txt_email, txt_password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(auth.getCurrentUser().getUid());
+
+                                        reference.addValueEventListener(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                progressDialog.dismiss();
+                                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                            }
+                                        });
+
+                                    } else {
+                                        progressDialog.dismiss();
+                                        Toast.makeText(LoginActivity.this, "Authentication Failed!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+        txt_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+   /* @Override
+    protected void onResume() {
+        super.onResume();
+        animationDrawable.start();
+    }*/
+}
---
+++ app/src/main/java/com/example/instashare/RegisterActivity.java
@@ -0,0 +1,131 @@
+package com.example.instashare;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText username, full_name,  email, password;
+    Button btn_register;
+    TextView txt_login;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        full_name = findViewById(R.id.full_name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        txt_login = findViewById(R.id.txt_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        auth = FirebaseAuth.getInstance();
+
+        txt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog = new ProgressDialog(RegisterActivity.this);
+                progressDialog.setMessage("Please wait..");
+                progressDialog.show();
+
+                String username_str = username.getText().toString();
+                String full_name_str = full_name.getText().toString();
+                String email_str = email.getText().toString();
+                String password_str = password.getText().toString();
+
+                if (    TextUtils.isEmpty(username_str) ||
+                        TextUtils.isEmpty(full_name_str) ||
+                        TextUtils.isEmpty(email_str) ||
+                        TextUtils.isEmpty(password_str) ) {
+
+                    Toast.makeText(RegisterActivity.this, "All fields are Required to Fill...", Toast.LENGTH_SHORT).show();
+                } else if (password_str.length() <6) {
+                    Toast.makeText(RegisterActivity.this, "Password must have 6 characters or more", Toast.LENGTH_SHORT).show();
+                } else  {
+                    register(username_str, full_name_str, email_str, password_str);
+                }
+
+            }
+        });
+    }
+
+    private void register(final String username, final String full_name, String email, String password) {
+        Log.i("REGISTER", "GOT Called");
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.i("SignUp", "GOT Completed");
+                    FirebaseUser fCurrentUser = auth.getCurrentUser();
+                    String uid = fCurrentUser.getUid();
+
+                    reference = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+                    Log.i("Reference", "GOT REFERENCE");
+
+                    HashMap<String, Object> hashMap = new HashMap<>();
+                    hashMap.put("id", uid);
+                    hashMap.put("username", username.toLowerCase());
+                    hashMap.put("fullname", full_name);
+                    hashMap.put("bio", "");
+                    hashMap.put("imageURL", "default");
+//                    hashMap.put("status", "offline");
+//                    hashMap.put("search", username.toLowerCase());
+
+
+                    reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                progressDialog.dismiss();
+                                Log.i("Database", "Database VALUE is SET");
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+
+                } else {
+                    progressDialog.dismiss();
+                    Toast.makeText(RegisterActivity.this, "Something went wrong,\nPlz check your Internet connection and Try Again!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
